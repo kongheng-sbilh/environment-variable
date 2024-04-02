fa1e2bd16b576ae3c04a41bcb4d50be08ff8c5a9
kongheng-sbilh <kongheng.long@sbilhbank.com.kh>
2024-04-02 09:48:55 +0700

    feat: init project

---
+++ src/test/java/com/kh/environmentvariable/EnvironmentVariableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.environmentvariable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentVariableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
